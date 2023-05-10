f1d1a1be79319ce7bc608542b655e6161399f2d1
Pau Bertolin <paulaberos@gmail.com>
2023-05-10 19:25:09 +0200

    added some style contact view

--- src/views/AboutUs.jsx
+++ src/views/AboutUs.jsx
@@ -11,22 +11,22 @@ const AboutUs = () => {
       <NavBar />
       <div className="main_view_container about_us">
         <div className='photo_caption'>
-        <img style={{width: "500px"}} src={profile} alt="Profile Gerard & Pau" />
+          <img style={{ width: "500px" }} src={profile} alt="Profile Gerard & Pau" />
           <figcaption>Paula Bertolin & Gerard Solanes</figcaption>
         </div>
         <div className="title_p_about_us">
-        <h1>ABOUT US</h1>
-        <p>
-          We are a team of two dedicated full-stack developers with a unique blend
-          of <b>programming</b>, <b>data analysis</b>, and <b>design</b> expertise. Specialising in the
-          MERN stack (MongoDB, Express, React, and Node.js) and WORDPRESS, we
-          bring our passion for innovation and problem-solving to every project.<br></br><br></br>
-          Driven by our passion for coding and excellence, we pride ourselves on
-          delivering engaging, visually striking, and functional web experiences
-          that resonate with users and elevate our clients&apos; brand presence. Build
-          the future with us!
+          <h1>ABOUT US</h1>
+          <p>
+            We are a team of two dedicated full-stack developers with a unique blend
+            of <b>programming</b>, <b>data analysis</b>, and <b>design</b> expertise. Specialising in the
+            MERN stack (MongoDB, Express, React, and Node.js) and WORDPRESS, we
+            bring our passion for innovation and problem-solving to every project.<br></br><br></br>
+            Driven by our passion for coding and excellence, we pride ourselves on
+            delivering engaging, visually striking, and functional web experiences
+            that resonate with users and elevate our clients&apos; brand presence. Build
+            the future with us!
           </p>
-          </div>
+        </div>
       </div>
     </div>
   );
--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -2,28 +2,37 @@ import NavBar from "../components/NavBar";
 import linkedIn from "../assets/images/linkedin_logo.png";
 import email from "../assets/images/email.png";
 import malt from "../assets/images/malt_logo.png";
+import Frame from "../components/Frame";
+import './contact.css';
 
 const Contact = () => {
   return (
-    <div className="contact">
+    <div className="main_view_container frame contact">
+      <Frame />
       <NavBar />
-      <h1>Contact</h1>
-      <p>
-        Tell us about your project, arrange a meeting with us and let&apos;s start
-        creating together.
-      </p>
-      <div>
-        <p>You can also contact us through:</p>
-        <img src={malt} alt="malt-icon" />
-        <img src={email} alt="email-icon" />
-        <img src={linkedIn} alt="linkedin-icon" />
+      <div className="title_text_social_container">
+        <h1>CONTACT</h1>
+        <p>
+          Tell us about your project, arrange a meeting with us and let&apos;s start
+          creating together.
+        </p>
+        <div className="social_container">
+          <p>You can also contact us through:</p>
+          <div className="icons">
+          <img src={malt} alt="malt-icon" />
+          <img src={email} alt="email-icon" />
+            <img src={linkedIn} alt="linkedin-icon" />
+            </div>
+        </div>
       </div>
-      <form>
-        <input type="text" placeholder="Name"/>
-        <input type="text" placeholder="Email"/>
-        <input type="text" placeholder="Message" />
-        <button>Send</button>
-      </form>
+      <div className="contact_form_container">
+        <form>
+          <input type="text" placeholder="NAME" />
+          <input type="text" placeholder="EMAIL" />
+          <textarea type="text" placeholder="MESSAGE" rows="8"/>
+          <button>SEND</button>
+        </form>
+      </div>
     </div>
   );
 };
