584338ca767367ca73bb5f41b137a1a99e26ff68
Gerard Solanes <gerardsolher@gmail.com>
2023-05-10 15:24:04 +0200

    Style architecture

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
+import './Home.css';
 import Frame from "../components/Frame";
+import blob from '../assets/blobs/home-blob.svg';
 
 const Home = () => {
   return (
@@ -12,6 +14,7 @@ const Home = () => {
         <li><Link to={'/contact'}>Contact</Link></li>
       </ul>
       <p>We are the full-stack developers who deliver impactful web solutions that resonate in the digital world</p>
+      <img className="homeBlob" src={blob} alt="Background blob" />
     </div>
   );
  }
