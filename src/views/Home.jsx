0b87addf77f0c1e64d6046551eb6d076d4560969
Gerard Solanes <gerardsolher@gmail.com>
2023-05-10 11:57:25 +0200

    Adds home content

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,8 +1,14 @@
-
+import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div className="home">
-      
+      <h1>QuoDev</h1>
+      <ul>
+        <li><Link to={'/projects'}>Projects</Link></li>
+        <li><Link to={'/about-us'}>About Us</Link></li>
+        <li><Link to={'/contact'}>Contact</Link></li>
+      </ul>
+      <p>We are the full-stack developers who deliver impactful web solutions that resonate in the digital world</p>
     </div>
   );
  }
