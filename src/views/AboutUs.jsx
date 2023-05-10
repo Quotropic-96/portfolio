543e5668f83a859980f0c682268744c06486e76d
Pau Bertolin <paulaberos@gmail.com>
2023-05-10 19:03:16 +0200

    added some style About Us view

--- src/views/AboutUs.jsx
+++ src/views/AboutUs.jsx
@@ -1,23 +1,33 @@
 import profile from '../assets/images/profile.png';
 import NavBar from '../components/NavBar';
+import './aboutUs.css';
+import Frame from '../components/Frame';
+
 
 const AboutUs = () => {
   return (
-    <div className="about_us">
+    <div className="frame">
+      <Frame />
       <NavBar />
-      <h1>About Us</h1>
-      <p>
-        We are a team of two dedicated full-stack developers with a unique blend
-        of programming, data analysis, and design expertise. Specialising in the
-        MERN stack (MongoDB, Express, React, and Node.js) and WORDPRESS, we
-        bring our passion for innovation and problem-solving to every project.
-        Driven by our passion for coding and excellence, we pride ourselves on
-        delivering engaging, visually striking, and functional web experiences
-        that resonate with users and elevate our clients&apos; brand presence. Build
-        the future with us!
-      </p>
-      <img src={profile} alt="Profile Gerard & Pau" />
-      <figcaption>Paula Bertolin & Gerard Solanes</figcaption>
+      <div className="main_view_container about_us">
+        <div className='photo_caption'>
+        <img style={{width: "500px"}} src={profile} alt="Profile Gerard & Pau" />
+          <figcaption>Paula Bertolin & Gerard Solanes</figcaption>
+        </div>
+        <div className="title_p_about_us">
+        <h1>ABOUT US</h1>
+        <p>
+          We are a team of two dedicated full-stack developers with a unique blend
+          of <b>programming</b>, <b>data analysis</b>, and <b>design</b> expertise. Specialising in the
+          MERN stack (MongoDB, Express, React, and Node.js) and WORDPRESS, we
+          bring our passion for innovation and problem-solving to every project.<br></br><br></br>
+          Driven by our passion for coding and excellence, we pride ourselves on
+          delivering engaging, visually striking, and functional web experiences
+          that resonate with users and elevate our clients&apos; brand presence. Build
+          the future with us!
+          </p>
+          </div>
+      </div>
     </div>
   );
 };
