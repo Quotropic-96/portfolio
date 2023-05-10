d37efc535242c03cd2441389201cadf490d31a02
Gerard Solanes <gerardsolher@gmail.com>
2023-05-10 12:12:05 +0200

    Adds contact content

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -1,10 +1,23 @@
-
 const Contact = () => {
   return (
     <div className="contact">
-      
+      <h1>Contact</h1>
+      <p>
+        Tell us about your project, arrange a meeting with us and let&apos;s start
+        creating together.
+      </p>
+      <div>
+        <p>You can also contact us through:</p>
+        {/* Social Links  */}
+      </div>
+      <form>
+        <input type="text" placeholder="Name"/>
+        <input type="text" placeholder="Email"/>
+        <input type="text" placeholder="Message" />
+        <button>Send</button>
+      </form>
     </div>
   );
- }
+};
 
 export default Contact;
