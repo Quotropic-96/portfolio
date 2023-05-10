5f7bc033bcb1f996e97d7916e189cea7a5d90d6a
Pau Bertolin <paulaberos@gmail.com>
2023-05-10 15:58:40 +0200

    added arrows

--- src/views/Projects.jsx
+++ src/views/Projects.jsx
@@ -2,21 +2,26 @@ import ProjectCard from "../components/ProjectCard";
 import { projectsData } from "../data/projectsData.js";
 import mobile from "../assets/images/mockup.png";
 import NavBar from "../components/NavBar";
+import Frame from "../components/Frame";
+import "./projects.css"
 
 const Projects = () => {
   return (
-    <div className="projects">
+    <div className="projects frame">
+      <Frame />
       <NavBar />
-      <h1>Projects</h1>
-      <div className="projects_cards_container">
-        {projectsData.map(project => {
-          return (
-            <ProjectCard key={project.title} info={project} />
-          )
-        })}
-      </div>
-      <div className="demo_container">
-        <img style={{ width: "300px" }} src={mobile} alt="mobile" />
+      <h1>PROJECTS</h1>
+      <div className="projects_mobile_container">
+        <div className="projects_cards_container">
+          {projectsData.map(project => {
+            return (
+              <ProjectCard key={project.title} info={project} />
+            )
+          })}
+        </div>
+        <div className="demo_container">
+          <img style={{ width: "300px" }} src={mobile} alt="mobile" />
+        </div>
       </div>
     </div>
   );
