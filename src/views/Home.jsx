75f00018ae767176dd4a38b395effe4de343639b
Pau Bertolin <paulaberos@gmail.com>
2023-05-15 17:06:58 +0200

    testing framer motion at Home view

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -27,24 +27,24 @@ const Home = () => {
   );
 
   return (
-    <div className="frame">
+    <motion.div className="frame" initial={{ opacity: 0}} animate={{ opacity: 1}} transition={{ delay: 1.5, duration:1.5}}>
       <Frame />
-      <div className="home" >
+      <motion.div className="home">
         {/* <Blob cssStyle='background_blob' background='#F9F7F5' fill='#000000' style={{ mixBlendMode: "screen" }} />
         <Blob cssStyle='background_blob' background='#F9F7F5' fill='#ED6A5A' style={{mixBlendMode:"screen"}}/> */}
         <div className="blob_text_div"></div>
         <p className="blob_text_1">We are the full-stack developers who deliver impactful web solutions that resonate in the digital world</p>
         <p className="blob_text_2">We are the full-stack developers who deliver impactful web solutions that resonate in the digital world</p>
         <div className="homeContent">
-          <h1 className="webTitle">QuoDev</h1>
+          <motion.h1 className="webTitle">QuoDev</motion.h1>
           <ul className="homeMenu">
             <li>{renderLink('/projects', 'Projects')}</li>
             <li>{renderLink('/about-us', 'About Us')}</li>
             <li>{renderLink('/contact', 'Contact')}</li>
           </ul>
         </div>
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
  }
 
