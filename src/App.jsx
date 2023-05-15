7f413d99a38b8cfbc6853314d22215fc1aa6c10f
Gerard Solanes <gerardsolher@gmail.com>
2023-05-15 16:58:09 +0200

    Removes tests

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import Projects from './views/Projects';
 import Contact from './views/Contact';
 import AboutUs from './views/AboutUs';
 import TestColor from './views/TestColor';
-import ProjectsTest from './views/simTests';
 
 function App() {
   return (
@@ -17,7 +16,6 @@ function App() {
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/test" element={<TestColor /> } />
-        <Route path="/testSim" element={<ProjectsTest /> } />
       </Routes>
     </div>
   )
