bab2b7cde2d8692be58c1e52c9d0ced1a943de50
Gerard Solanes <gerardsolher@gmail.com>
2023-05-18 20:28:56 +0200

    Defines animations for project cards

---
+++ src/animations/projectCardsAnimations.js
@@ -0,0 +1,21 @@
+export default {
+  card: {
+    hidden: {
+      opacity: 0,
+    },
+    animate: {
+      opacity: 1,
+      transition: {
+        ease: "easeInOut",
+        duration: .5,
+      },
+    },
+    exit: {
+      opacity: 0,
+      transition: {
+        ease: "easeInOut",
+        duration: .5,
+      },
+    },
+  },
+};
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,11 +1,12 @@
 import "./projectCard.css";
 import PropTypes from "prop-types";
 import leftArrow from '../assets/icons/arrow_left.png';
-import downArrow from '../assets/icons/arrow_down.png'
+import downArrow from '../assets/icons/arrow_down.png';
+import { motion } from "framer-motion";
+import animations from '../animations/projectCardsAnimations';
 
 const ProjectCard = ({ project, selectedTitle, handleSelectTitle, handleClose, handleScrollColor }) => {
   const { title, subtitle, about, show, color } = project;
-
   const handleClick = (color) => {
     if (selectedTitle) {
       handleClose();
@@ -14,25 +15,39 @@ const ProjectCard = ({ project, selectedTitle, handleSelectTitle, handleClose, h
       handleSelectTitle(title);
       handleScrollColor(color);
     }
-  }
+  };
 
   return (
     <div className="project_card_container">
       <div className="title_arrow_container">
         <h2>{title}</h2>
-        <img className={!selectedTitle ? "left_arrow" : "down_arrow"} src={!selectedTitle ? leftArrow : downArrow} alt="left arrow" onClick={() => handleClick(color)} />
+        <img
+          className={!selectedTitle ? "left_arrow" : "down_arrow"}
+          src={!selectedTitle ? leftArrow : downArrow}
+          alt="left arrow"
+          onClick={() => handleClick(color)}
+        />
       </div>
       <h3 style={{ color: selectedTitle && `${color}` }}>{subtitle}</h3>
-      {selectedTitle &&
-        <div className="about_show_div">
+      {selectedTitle && (
+        <motion.div className="about_show_div"
+          key={title}
+          variants={animations.card}
+          initial="hidden"
+          animate="animate"
+          exit="exit"
+        >
           <p>{about}</p>
 
-          <p className={color === "#015CA0" ? "show_p white" : "show_p"} style={{ backgroundColor: `${color}`}} >{show}</p>
-    
-        </div>
-      }
-      <div className="projects_cards_container">
-      </div>
+          <p
+            className={color === "#015CA0" ? "show_p white" : "show_p"}
+            style={{ backgroundColor: `${color}` }}
+          >
+            {show}
+          </p>
+        </motion.div>
+      )}
+      <div className="projects_cards_container"></div>
     </div>
   );
 }
