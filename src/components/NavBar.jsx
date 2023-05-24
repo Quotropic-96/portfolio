7f614f8a81df139c1ebf743bd947c5d4185b4921
Gerard Solanes <gerardsolher@gmail.com>
2023-05-24 10:01:50 +0200

    Styles navbar for mobile

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,25 @@
 import { NavLink, useLocation } from "react-router-dom";
+import { useState, useEffect } from 'react';
 import { motion } from "framer-motion";
-import animations from "../animations/navbarAnimations";
+import animations from '../animations/navbarAnimations';
 
 const NavBar = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const location = useLocation();
 
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
-    <motion.nav
-      className="navbar"
-      key={"navbar"}
+    <motion.nav className="navbar"
+      key={'navbar'}
       variants={animations.navbar}
       initial="hidden"
       animate="animate"
@@ -16,28 +27,28 @@ const NavBar = () => {
     >
       <ul>
         <li>
-          <NavLink to={"/"}>
-            {location.pathname === "/" ? "•" : "<Home />"}
+          <NavLink to={'/'} activeClassName="active">
+            {location.pathname === '/' && windowWidth <= 1000 ? '•' : '<Home />'}
           </NavLink>
         </li>
         <li>
-          <NavLink to={"/projects"}>
-            {location.pathname === "/projects" ? "•" : "<Projects />"}
+          <NavLink to={'/projects'} activeClassName="active">
+            {location.pathname === '/projects' && windowWidth <= 1000 ? '•' : '<Projects />'}
           </NavLink>
         </li>
         <li>
-          <NavLink to={"/about-us"}>
-            {location.pathname === "/about-us" ? "•" : "<About Us />"}
+          <NavLink to={'/about-us'} activeClassName="active">
+            {location.pathname === '/about-us' && windowWidth <= 1000 ? '•' : '<About Us />'}
           </NavLink>
         </li>
         <li>
-          <NavLink to={"/contact"}>
-            {location.pathname === "/contact" ? "•" : "<Contact />"}
+          <NavLink to={'/contact'} activeClassName="active">
+            {location.pathname === '/contact' && windowWidth <= 1000 ? '•' : '<Contact />'}
           </NavLink>
         </li>
       </ul>
     </motion.nav>
-  );
-};
+  )
+}
 
 export default NavBar;
