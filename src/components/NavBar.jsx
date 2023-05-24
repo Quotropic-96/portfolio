e6d66e637c9e594249525a6e48bd2805c8ccba53
Pau Bertolin <paulaberos@gmail.com>
2023-05-24 16:00:39 +0200

    added active bullet

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,6 +2,7 @@ import { NavLink, useLocation } from "react-router-dom";
 import { useState, useEffect } from 'react';
 import { motion } from "framer-motion";
 import animations from '../animations/navbarAnimations';
+import activeBullet from '../assets/icons/item-menu-active.png';
 
 const NavBar = () => {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
@@ -28,22 +29,22 @@ const NavBar = () => {
       <ul>
         <li>
           <NavLink to={'/'}>
-            {location.pathname === '/' && windowWidth <= 1000 ? '•' : '<Home />'}
+            {location.pathname === '/' && windowWidth <= 1000 ? <img className="active-bullet" src={activeBullet} alt="active bullet" /> : '<Home />'}
           </NavLink>
         </li>
         <li>
           <NavLink to={'/projects'}>
-            {location.pathname === '/projects' && windowWidth <= 1000 ? '•' : '<Projects />'}
+            {location.pathname === '/projects' && windowWidth <= 1000 ? <img className="active-bullet" src={activeBullet} alt="active bullet" /> : '<Projects />'}
           </NavLink>
         </li>
         <li>
           <NavLink to={'/about-us'}>
-            {location.pathname === '/about-us' && windowWidth <= 1000 ? '•' : '<About Us />'}
+            {location.pathname === '/about-us' && windowWidth <= 1000 ? <img className="active-bullet" src={activeBullet} alt="active bullet" /> : '<About Us />'}
           </NavLink>
         </li>
         <li>
           <NavLink to={'/contact'}>
-            {location.pathname === '/contact' && windowWidth <= 1000 ? '•' : '<Contact />'}
+            {location.pathname === '/contact' && windowWidth <= 1000 ? <img className="active-bullet" src={activeBullet} alt="active bullet" /> : '<Contact />'}
           </NavLink>
         </li>
       </ul>
